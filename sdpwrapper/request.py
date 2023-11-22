ca452327d43f33607fbe205a734c7e83fba8e03d
Sergey Gambaryan <sgamb2000@gmail.com>
2023-11-22 22:36:31 +0400

    Fix problem with subject

--- sdpwrapper/request.py
+++ sdpwrapper/request.py
@@ -1,6 +1,7 @@
 class Request():
     def __init__(self, id):
         self.id = id
+        self.subject = 'Разработка пайтон обертки для sdp/sc api'
 
     def info(self):
-        return {'id': self.id}
+        return {'id': self.id, 'subject': self.subject}
